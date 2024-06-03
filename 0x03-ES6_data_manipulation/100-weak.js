e78a439870ec382fc709e8d39d1e32475de0a4ce
Jeanluchabi <jeanuchabi@gmail.com>
2024-06-03 21:45:00 +0200

    some changes in 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,28 +1,13 @@
-// Defining a const instance of WeakMap and naming it weakMap
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-// Exporting the weakMap constant
-export { weakMap };
+const MAX_ENDPOINT_CALLS = 5;
 
-// Define the queryAPI function
-function queryAPI(endpoint) {
-  // Check if the endpoint is already tracked in the weakMap
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    // If not, initialize the count to 1
-    weakMap.set(endpoint, 1);
-  } else {
-    // If it is, increment the count by 1
-    const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
-    
-    // Check if the number of queries is >= 5
-    if (count + 1 >= 5) {
-      // If it is, throw an error
-      throw new Error('Endpoint load is high');
-    }
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
   }
 }
-
-// Exporting the queryAPI function as default
-export default queryAPI;
-
